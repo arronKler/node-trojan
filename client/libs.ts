8c5449b9f24a9f4aee81eb67370a324284a860f4
arronKler <panzer9@126.com>
2020-03-21 00:58:24 +0800

    feat: run test of socks5 client

--- client/libs.ts
+++ client/libs.ts
@@ -1,4 +1,7 @@
 import net from 'net'
+import tls from 'tls'
+import fs from 'fs'
+import crypto from 'crypto'
 import { AuthMethods, CommandType, AddressType } from './enums'
 import Shake from './classes/Shake'
 
@@ -51,11 +54,14 @@ export function handleRequest(socket: net.Socket) {
     console.log('port: %j', port)
     console.log('address: %j', address)
     if (buff[1] === CommandType.TCPConnect) {
-      let proxy = net.createConnection(port, address, () => {
-        let resp = Buffer.alloc(buff.length)
-        buff.copy(resp)
-        resp[1] = 0x00
-        socket.write(resp)
+      let resp = Buffer.alloc(buff.length)
+      buff.copy(resp)
+      resp[1] = 0x00
+      socket.write(resp)
+
+      createServerConn(socket, port, address)
+      /* let proxy = net.createConnection(port, address, () => {
+        
 
         proxy.pipe(socket)
         socket.pipe(proxy)
@@ -64,10 +70,67 @@ export function handleRequest(socket: net.Socket) {
         console.log('time outed')
         socket.destroy()
         proxy.destroy()
-      })
+      }) */
     } else {
       console.log('not tcp')
       socket.end(buff)
     }
+  })
+}
+
+const options: tls.ConnectionOptions = {
+  ca: fs.readFileSync('./cert/server-cert.pem'),
+  host: 'localhost',
+  port: 8000
+}
+
+function createServerConn(
+  _socket: net.Socket,
+  _port: number,
+  _address: string
+) {
+  const socket = tls.connect(options, () => {
+    const hash = crypto.createHash('sha224')
+    hash.update('arronkler', 'utf8')
+    let passwd = Buffer.from(hash.digest('hex'))
+    console.log('passwd: ', passwd.toString())
+    let startBuff = Buffer.from([0x0d, 0x0a])
+    let endBuff = Buffer.from([0x0d, 0x0a])
+
+    let cmd = Buffer.from([0x01])
+    let atyp = Buffer.from([0x03])
+    let dst = Buffer.from('127.0.0.1')
+    let port = Buffer.from(`${_port}`)
+    let requestBuff = Buffer.concat([cmd, atyp, dst, port])
+
+    let payload = Buffer.from('AAAAA')
+
+    let totalBuff = Buffer.concat([
+      passwd,
+      startBuff,
+      requestBuff,
+      endBuff,
+      payload
+    ])
+
+    console.log(totalBuff.byteLength, startBuff.byteLength, cmd.byteLength)
+    console.log(port.toString(), port.byteLength)
+
+    // socket.write(totalBuff)
+    socket.write(Buffer.from('gjoshg'))
+    socket.once('data', chunk => {
+      console.log('first chunk:', chunk.toString('utf8'))
+      if (chunk.toString('utf8') == 'confirm') {
+        // process.stdin.pipe(socket)
+        socket.end('GET / HTTP/1.0\n\n')
+        socket.on('data', chunk => {
+          console.log('get chunk', chunk.toString('utf8'))
+        })
+
+        socket.on('error', err => {
+          console.error(err)
+        })
+      }
+    })
   })
 }
